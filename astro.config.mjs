a5e1aace8f3158e3a0772d45082e45f1c4306035
RiccardoFracchioni <riccardo.fracchioni@gmail.com>
2026-07-28 12:03:25 +0200

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({site: 'https://riccardofracchioni.github.io'});
+export default defineConfig({
+  site: "https://riccardofracchioni.github.io",
+  integrations: [sitemap()],
+});
